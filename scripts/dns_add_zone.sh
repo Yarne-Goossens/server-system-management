b3dc788f2c2bb596670d5055deee3ccfa07bb49d
Yarne-Goossens <yarne.goossens@student.ucll.be>
2023-11-19 19:09:26 +0100

    added zone config to script

--- scripts/dns_add_zone.sh
+++ scripts/dns_add_zone.sh
@@ -3,10 +3,23 @@
 subzone=$1
 nameserver="yarne-goossens.sasm.uclllabs.be"
 ns_record="ns.$subzone.$nameserver"
-zone_config='zone "'$subzone'.yarne-goossens.sasm.uclllabs.be" {
+dns_config='zone "'$subzone'.yarne-goossens.sasm.uclllabs.be" {
     type master;
     file "/etc/bind/zones/db.'$subzone'.yarne-goossens.sasm.uclllabs.be";
 };'
+zone_config='$TTL    300
+@       IN      SOA     ns.'$subzone'.yarne-goossens.sasm.uclllabs.be. admin.yarne-goossens.sasm.uclllabs.be. (
+                            '"$(date +'%Y%m%d%H%M%S')"'  ; Serial
+                            300         ; Refresh
+                            300         ; Retry
+                            300         ; Expire
+                            300 )       ; Negative Cache TTL
+;
+@       IN      NS      ns.yarne-goossens.sasm.uclllabs.be.
+
+@       IN      A       193.191.176.67
+ns      IN      A       193.191.176.67'
+
 
 if [ -z "$1" ]
 then
@@ -17,4 +30,6 @@ fi
 echo "$subzone"
 echo "$nameserver"
 echo "$ns_record"
+echo "$dns_config"
 echo "$zone_config"
+
