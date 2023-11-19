208de2549ec85b388d7e2ece221ec4445156e839
Yarne-Goossens <yarne.goossens@student.ucll.be>
2023-11-19 17:33:00 +0100

    added check to see if there's an argument supplied

--- scripts/dns_add_zone.sh
+++ scripts/dns_add_zone.sh
@@ -4,6 +4,12 @@ subzone=$1
 nameserver="yarne-goossens.sasm.uclllabs.be"
 ns_record="ns.$subzone.$nameserver"
 
+if [ -z "$1" ]
+then
+  echo "No argument supplied"
+  exit 1
+fi
+
 echo "$subzone"
 echo "$nameserver"
 echo "$ns_record"
