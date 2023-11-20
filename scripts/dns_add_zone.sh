1bf1e828f7357cd8ba6ce23ae00ba44e193554d6
Yarne-Goossens <yarne.goossens@student.ucll.be>
2023-11-20 18:10:35 +0100

    script seems to be almost done, only need to fix serial now

--- scripts/dns_add_zone.sh
+++ scripts/dns_add_zone.sh
@@ -3,23 +3,6 @@
 subzone=$1
 nameserver="yarne-goossens.sasm.uclllabs.be"
 ns_record="ns.$subzone.$nameserver"
-dns_config="zone "'$subzone'.'$nameserver'" {
-    type master;
-    file "/etc/bind/zones/db.'$subzone'.'$nameserver'";
-};"
-zone_config="$TTL    300
-@       IN      SOA     ns.'$subzone'.'$nameserver'. admin.'$nameserver'. (
-                            '"$(date +'%Y%m%d%H%M%S')"'  ; Serial
-                            300         ; Refresh
-                            300         ; Retry
-                            300         ; Expire
-                            300 )       ; Negative Cache TTL
-;
-@       IN      NS      ns.'$subzone'.'$nameserver'.
-@       IN      NS      ns.'$nameserver'.
-
-@       IN      A       193.191.176.67
-ns      IN      A       193.191.176.67"
 
 
 if [ -z "$1" ]
@@ -28,15 +11,34 @@ then
   exit 1
 fi
 
-echo "$subzone"
-echo "$nameserver"
-echo "$ns_record"
-echo "$dns_config"
-echo "$zone_config"
 
-echo -e $zone_config >> /etc/bind/zones/db.$subzone.$nameserver
+cat << EOF >> /etc/bind/zones/db.$subzone.$nameserver
+\$TTL    300
+@       IN      SOA     ns.$subzone.$nameserver. admin.$nameserver. (
+                    $(date +'%Y%m%d%H%M%S')  ; Serial
+                            300         ; Refresh
+                            300         ; Retry
+                            300         ; Expire
+                            300 )       ; Negative Cache TTL
+;
+@       IN      NS      ns.$subzone.$nameserver.
+@       IN      NS      ns.$nameserver.
 
-echo -e $dns_config >> /etc/bind/named.conf.yoda-zones
+@       IN      A       193.191.176.67
+ns      IN      A       193.191.176.67
+
+EOF
+
+echo "Zone file made"
+
+cat << EOF >> /etc/bind/named.conf.yoda-zones
+zone "$subzone.$nameserver" {
+    type master;
+    file "/etc/bind/zones/db.$subzone.$nameserver";
+};
+EOF
+
+echo "Zone added to named.conf.yoda-zones"
 
 serial=$(("$(date +'%Y%m%d%H%M%S')"))
 end=$(grep -Po '\s+; Serial' /etc/bind/zones/db.$nameserver)
@@ -44,8 +46,11 @@ soa=$(grep -Po '\d+\s+; Serial' /etc/bind/zones/db.$nameserver)
 
 sed -i "s/$soa/$serial$end/g" /etc/bind/zones/db.$nameserver
 
-echo -e "$subzone       IN NS ns.'$nameserver'" >> /etc/bind/zones/db.$nameserver
+cat << EOF >> /etc/bind/zones/db.$nameserver
+$subzone  IN NS ns.$nameserver.
+EOF
 
 systemctl restart bind9
 
-echo "done"
+echo "Done"
+
