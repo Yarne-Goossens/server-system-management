d8e47f859f3af08fe4c576daf99aa6f4816fcc4c
Yarne-Goossens <yarne.goossens@student.ucll.be>
2023-11-20 17:45:02 +0100

    almost working add zone script

--- scripts/dns_add_zone.sh
+++ scripts/dns_add_zone.sh
@@ -3,23 +3,23 @@
 subzone=$1
 nameserver="yarne-goossens.sasm.uclllabs.be"
 ns_record="ns.$subzone.$nameserver"
-dns_config='zone "'$subzone'.yarne-goossens.sasm.uclllabs.be" {
+dns_config="zone "'$subzone'.'$nameserver'" {
     type master;
-    file "/etc/bind/zones/db.'$subzone'.yarne-goossens.sasm.uclllabs.be";
-};'
-zone_config='$TTL    300
-@       IN      SOA     ns.'$subzone'.yarne-goossens.sasm.uclllabs.be. admin.yarne-goossens.sasm.uclllabs.be. (
+    file "/etc/bind/zones/db.'$subzone'.'$nameserver'";
+};"
+zone_config="$TTL    300
+@       IN      SOA     ns.'$subzone'.'$nameserver'. admin.'$nameserver'. (
                             '"$(date +'%Y%m%d%H%M%S')"'  ; Serial
                             300         ; Refresh
                             300         ; Retry
                             300         ; Expire
                             300 )       ; Negative Cache TTL
 ;
-@       IN      NS      ns.'$subzone'.yarne-goossens.sasm.uclllabs.be.
-@       IN      NS      ns.yarne-goossens.sasm.uclllabs.be.
+@       IN      NS      ns.'$subzone'.'$nameserver'.
+@       IN      NS      ns.'$nameserver'.
 
 @       IN      A       193.191.176.67
-ns      IN      A       193.191.176.67'
+ns      IN      A       193.191.176.67"
 
 
 if [ -z "$1" ]
@@ -34,9 +34,18 @@ echo "$ns_record"
 echo "$dns_config"
 echo "$zone_config"
 
-bash -c "echo -e '$zone_config' >> /etc/bind/zones/db.'$subzone'.yarne-goossens.sasm.uclllabs.be"
-chown bind:bind "/etc/bind/zones/db.'$subzone'.yarne-goossens.sasm.uclllabs.be"
+echo -e $zone_config >> /etc/bind/zones/db.$subzone.$nameserver
 
-bash -c "echo -e '$dns_config' >> /etc/bind/named.conf.yoda-zones"
+echo -e $dns_config >> /etc/bind/named.conf.yoda-zones
+
+serial=$(("$(date +'%Y%m%d%H%M%S')"))
+end=$(grep -Po '\s+; Serial' /etc/bind/zones/db.$nameserver)
+soa=$(grep -Po '\d+\s+; Serial' /etc/bind/zones/db.$nameserver)
+
+sed -i "s/$soa/$serial$end/g" /etc/bind/zones/db.$nameserver
+
+echo -e "$subzone       IN NS ns.'$nameserver'" >> /etc/bind/zones/db.$nameserver
+
+systemctl restart bind9
 
 echo "done"
