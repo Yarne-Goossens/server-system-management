2820d8ab4c16e52f7d53b7e252e65d23751cfb05
Yarne-Goossens <yarne.goossens@student.ucll.be>
2023-11-21 20:27:39 +0100

    http seems to be working

--- scripts/http_add_vhost.sh
+++ scripts/http_add_vhost.sh
@@ -9,31 +9,32 @@ fqdn=$1
 
 result=$(nslookup "$fqdn")
 
-echo "$result"
-
 if [[ $result == *"can't find"* ]]; then
   echo "Subdomain does not exist"
+  exit 1
 else
   echo "Subdomain exists"
 fi
 
-document_root="/var/www/html/$fqdn"
+document_root="/var/www/html/yoda_$fqdn"
 
-#mkdir "$document_root"
+mkdir "$document_root"
 
-echo "<VirtualHost *:80>"
-echo "        ServerAdmin webmaster@$fqdn"
-echo "        DocumentRoot $document_root"
-echo "        ServerName $fqdn"
-echo "        ServerAlias www.$fqdn"
-echo "        ErrorLog ${APACHE_LOG_DIR}/${fqdn}_error.log"
-echo "        CustomLog ${APACHE_LOG_DIR}/${fqdn}_access.log combined"
-echo "</VirtualHost>"
+cat << EOF > "/etc/apache2/sites-available/yoda_${fqdn}.conf"
+<VirtualHost *:80>
+        ServerAdmin webmaster@$fqdn
+        DocumentRoot $document_root
+        ServerName $fqdn
+        #ServerAlias www.$fqdn
+        ErrorLog ${APACHE_LOG_DIR}/yoda_${fqdn}_error.log
+        CustomLog ${APACHE_LOG_DIR}/yoda_${fqdn}_access.log combined
+</VirtualHost>
+EOF
 
-echo "welcome $fqdn" #> "$document_root/index.html"
-echo "welcome www.$fqdn" #> /var/www/html/www.$fqdn/index.html
+echo "welcome $fqdn" > "$document_root/index.html"
+#echo "welcome www.$fqdn" > /var/www/html/www.$fqdn/index.html
 
-#a2ensite "$fqdn.conf"
+a2ensite "yoda_${fqdn}.conf"
 
-#systemctl reload apache2
+systemctl reload apache2
 
