7846efa5bb71ba9a606a23cf36159e26436cc54b
Yarne-Goossens <yarne.goossens@student.ucll.be>
2023-11-21 19:31:29 +0100

    start of http script

---
+++ scripts/http_add_vhost.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Add a FQDN"
+    exit 1
+fi
+
+fqdn=$1
+echo "$fqdn"
+
+document_root="/var/www/html/$fqdn"
+
+echo "$document_root"
+#mkdir "$document_root"
+
+echo "<VirtualHost *:80>"
+echo "        ServerAdmin webmaster@$fqdn"
+echo "        DocumentRoot $document_root"
+echo "        ServerName $fqdn"
+echo "        ServerAlias www.$fqdn"
+echo "        ErrorLog ${APACHE_LOG_DIR}/$fqdn_error.log"
+echo "        CustomLog ${APACHE_LOG_DIR}/$fqdn_access.log combined"
+echo "</VirtualHost>"
